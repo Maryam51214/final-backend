dd400c026f108ba02163274dda878a252797dd85
Maryam51214 <maryam.shah@gmail.com>
2024-09-20 18:08:46 +0500

    first commit

--- db/conn.js
+++ db/conn.js
@@ -1,32 +1,18 @@
-// import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-// const Connection = async () => {
-//     try {
-//         await mongoose.connect(process.env.DB_URL, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//         });
-//         console.log("Database connected successfully");
-//     } catch (error) {
-//         console.error("Database connection error: ", error.message);
-//         process.exit(1); // Exit the process if the database connection fails
-//     }
-// };
+dotenv.config();
 
-// export default Connection;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connected');
+  } catch (error) {
+    console.error('Database connection error:', error);
+  }
+};
 
-
-import mongoose from "mongoose";
-
-const Connection = async () => {
-    try {
-        await mongoose.connect(process.env.db_url)
-        console.log("database connected");
-        
-    } catch (error) {
-        console.log(error);
-        
-    }
-}
-
-export default Connection;
+export default connectDB;
