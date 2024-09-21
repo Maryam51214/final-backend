9f001d5066764f19e58940faaf1424f4bccd3ecb
Maryam51214 <maryam.shah@gmail.com>
2024-09-22 03:11:51 +0500

    working on it

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { createFood, deleteFood, foodDetail, getFood, updateFood } from "../controllers/foodController.js";
 
-
+// Routing
 const foodRouter = express.Router();
+
 foodRouter.post("/create-food", createFood)
+
 foodRouter.get("/get-food", getFood)
+
 foodRouter.get("/food-detail/:id", foodDetail)
+
 foodRouter.put("/update-food/:id", updateFood)
+
 foodRouter.delete("/delete-food/:id", deleteFood)
 
 export default foodRouter;
